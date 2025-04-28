0d8d8218cb32f50f47abf7dc8fd0e3156dfce2c1
sathyaraj durai <sathyaraj.durai@securitybenefit.com>
2025-04-28 07:55:58 +0530

    remove

--- configureansiblewindows.ps1
+++ configureansiblewindows.ps1
@@ -1,6 +1,10 @@
-cd C:\
+New-Item -Path 'C:\temp\ansible' -ItemType Directory
+cd C:\temp\ansible
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass 
 Invoke-WebRequest https://raw.githubusercontent.com/sasudsha/powershell-script/refs/heads/main/DisableESC.ps1 -OutFile disableesc.ps1 
 .\disableesc.ps1 
 Invoke-WebRequest https://raw.githubusercontent.com/AlbanAndrieu/ansible-windows/refs/heads/master/files/ConfigureRemotingForAnsible.ps1 -OutFile ansible.ps1 
 .\ansible.ps1
+Invoke-WebRequest https://raw.githubusercontent.com/sasudsha/powershell-script/refs/heads/main/EnableESC.ps1 -OutFile enableesc.ps1
+.\enableesc.ps1
+Remove-Item -Path C:\temp\ansible\*.*
